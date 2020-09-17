9b3b59584a4877b51afe941906e1a3d470d7d2c4
worldmaster <glaubercold53@gmail.com>
2020-09-16 21:15:26 -0300

    Updates

---
+++ precache-manifest.357e7164ee5bb01d5c04c9487f571238.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b8703438e73cfca1beea54e609fc5755",
+    "url": "/monsters-roledex/index.html"
+  },
+  {
+    "revision": "7a8a2b9ea8c1c53492ee",
+    "url": "/monsters-roledex/static/css/main.ba882a84.chunk.css"
+  },
+  {
+    "revision": "524165774fdab1c62051",
+    "url": "/monsters-roledex/static/js/2.8597800b.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/monsters-roledex/static/js/2.8597800b.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "7a8a2b9ea8c1c53492ee",
+    "url": "/monsters-roledex/static/js/main.cdec5a9e.chunk.js"
+  },
+  {
+    "revision": "90f334eb0adc48655bcd",
+    "url": "/monsters-roledex/static/js/runtime-main.8b0015e6.js"
+  }
+]);
